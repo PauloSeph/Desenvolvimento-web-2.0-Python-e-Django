c2061b483eda96cff4e7b6f6c330ea5436f92f74
Seph <paulo.seph@hotmail.com>
2019-12-22 19:36:00 -0300

    Iniciando python com Django

---
+++ Python/Python/aula1.py
@@ -0,0 +1,19 @@
+print('Hello world')
+
+print(3+3)
+
+
+class Myclass():
+    a = 10
+    b = 3
+    a + b
+    print(a+b)
+
+    if 1 > 0:
+        a = a - 1
+        b = a + 10
+
+        try:
+            pass
+        except Exception as e:
+            raise e
---
+++ Python/Python/aula9.py
@@ -0,0 +1,18 @@
+condicao = True
+
+# while condicao:
+#     print('Executou')
+    
+# else:
+#         print('Fim do jogo')
+
+
+
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i = i + 1
+else:
+    print('Fim do loop')
